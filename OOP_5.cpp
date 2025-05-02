f50b9dc5266064ab444aa88ead4f961a3a5180df
Gr1nis <egor.bel2016@gmail.com>
2025-05-02 21:44:25 +0300

    Add files via upload

---
+++ OOP_5.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+
+// Шаблонный класс Queue
+template <typename T>
+class Queue {
+private:
+    std::queue<T> data;
+
+public:
+    // Добавление элемента в очередь
+    void push(const T& item) {
+        data.push(item);
+        std::cout << "Pushed: " << item << std::endl;
+    }
+
+    // Удаление элемента из очереди
+    void pop() {
+        if (!data.empty()) {
+            std::cout << "Popped: " << data.front() << std::endl;
+            data.pop();
+        }
+        else {
+            std::cout << "Queue is empty, nothing to pop." << std::endl;
+        }
+    }
+
+    // Просмотр первого элемента
+    void front() const {
+        if (!data.empty()) {
+            std::cout << "Front: " << data.front() << std::endl;
+        }
+        else {
+            std::cout << "Queue is empty." << std::endl;
+        }
+    }
+
+    // Проверка, пуста ли очередь
+    bool isEmpty() const {
+        return data.empty();
+    }
+};
+
+// Проверка работы шаблона
+int main() {
+    std::cout << "--- line queue ---" << std::endl;
+    Queue<std::string> stringQueue;
+    stringQueue.push("Apple");
+    stringQueue.push("Banana");
+    stringQueue.front();
+    stringQueue.pop();
+    stringQueue.front();
+
+    std::cout << "\n--- queue of numbers ---" << std::endl;
+    Queue<int> intQueue;
+    intQueue.push(10);
+    intQueue.push(20);
+    intQueue.front();
+    intQueue.pop();
+    intQueue.front();
+
+    return 0;
+}
